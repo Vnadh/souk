d19d56153f9b0765c76ff98770ea769957790c27
Vnadh <106485321+Vnadh@users.noreply.github.com>
2024-03-25 23:24:03 +0530

    mian

--- admin.php
+++ admin.php
@@ -27,7 +27,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Souk Admin Panel</title>
     <style>
         table {
             width: 100%;
